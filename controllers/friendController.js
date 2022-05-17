d9b34906547d6ed244a1e4298433aec61ccf3168
jadehuynh <jadehuynh08@gmail.com>
2022-05-17 07:52:14 -0700

    added friend controller

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -1,38 +1,32 @@
-const { Thoughts } = require('../models');
+const { Friends } = require('../models');
 
 module.exports = {
-  // Get all users
-  getThought(req, res) {
-    Thoughts.find()
+  // Get all friends
+  getFriend(req, res) {
+    Friends.find()
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
-  // Get a single user
-  getSingleThought(req, res) {
-    Thoughts.findOne({ _id: req.params.thoughtsId })
+  // Get a single friend
+  getSingleFriend(req, res) {
+    Friends.findOne({ _id: req.params.friendsId })
       .select('-__v')
       .then((user) =>
         !user
-          ? res.status(404).json({ message: 'No thought with that ID' })
+          ? res.status(404).json({ message: 'No friend with that ID' })
           : res.json(user)
       )
       .catch((err) => res.status(500).json(err));
   },
-  // create a new user
-  createThought(req, res) {
-    Thoughts.create(req.body)
-      .then((user) => res.json(user))
-      .catch((err) => res.status(500).json(err));
-  },
-  // Delete a user and associated apps
-  deleteThought(req, res) {
-    Thoughts.findOneAndDelete({ _id: req.params.userId })
+  // Delete a friend
+  deleteFriend(req, res) {
+    Friends.findOneAndDelete({ _id: req.params.userId })
       .then((user) =>
         !user
-          ? res.status(404).json({ message: 'No user with that ID to delete' })
-          : res.status(200).json({ message: 'Thought deleted' })
+          ? res.status(404).json({ message: 'No friend with that ID to delete' })
+          : res.status(200).json({ message: 'Friend deleted' })
       )
-      .then(() => res.json({ message: 'Thought deleted!' }))
+      .then(() => res.json({ message: 'Friend deleted!' }))
       .catch((err) => res.status(500).json(err));
   },
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -24,13 +24,14 @@ module.exports = {
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
+  
   // Delete a user
   deleteUser(req, res) {
     User.findOneAndDelete({ _id: req.params.userId })
       .then((user) =>
         !user
           ? res.status(404).json({ message: 'No user with that ID' })
-          : Application.deleteMany({ _id: { $in: user.applications } })
+          : Thoughts.deleteMany({ _id: { $in: user.thought } })
       )
       .then(() => res.json({ message: 'User deleted!' }))
       .catch((err) => res.status(500).json(err));
